5cdce04a9b08043fea7798355eedddb36b292132
FrancescoRizzi00 <rizzi.francesco@panettipitagora.edu.it>
2022-11-23 20:58:34 +0100

    Prove

---
+++ sito/static/js/index.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    
+function colore1(colore)
+{
+	 document.div.sfondo = "#00ff00"
+}
+
+function colore2(name){
+document.getElementById('div').style.backgroundColor = "#00ff00"
+}
+
+</script>
